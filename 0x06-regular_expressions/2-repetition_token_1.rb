e8906f9eea49f21eec78fe6a993e3793db61187e
Nwamanna <nwamannazika@gmail.com>
2023-05-03 18:20:38 +0100

    regex

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 word = ARGV[0]
 pattern = /hb{0,1}tn/
-word_match = word.match(pattern)
+word_match = word.scan(pattern)
 if word_match
-        puts word_match.join
+        puts word_match.join("")
 end
