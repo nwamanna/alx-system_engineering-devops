77e87d9cce550393c9f60b848bc41cd43a3ef340
Nwamanna <nwamannazika@gmail.com>
2023-07-24 21:24:06 +0100

    Inyang Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
 """This script returns to-do list information for a given employee ID."""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    id = int(argv[1])
+    url = "https://jsonplaceholder.typicode.com/"
+    user_dict = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo_dict = requests.get(url + "todos",
+                             params={"userId": sys.argv[1]}).json()
 
-    url = "https://jsonplaceholder.typicode.com/todos"
-    url2 = "https://jsonplaceholder.typicode.com/users"
-
-    connect = requests.get(url).json()
-    user = requests.get(url2).json()
-
-    lst = []
-    count = 0
-    for done in connect:
-        if done['userId'] == id and done['completed'] is True:
-            lst.append(done['title'])
-            count += 1
-    task = 0
-    for x in connect:
-        if x['userId'] == id:
-            task += 1
-
-    for name in user:
-        if name['id'] == id:
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name['name'], count, task))
-
-    for x in lst:
-        print("\t{}".format(x))
+    completed = [t.get("title") for t in
+                 todo_dict if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_dict.get("name"), len(completed), len(todo_dict)))
+    [print("\t {}".format(c)) for c in completed]
