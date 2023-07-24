4958b6dabd670cd0500dadda97eb6ae1887772ed
Nwamanna <nwamannazika@gmail.com>
2023-07-24 21:00:45 +0100

    fix pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,9 +6,11 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_dict = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todo_dict = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todo_dict = requests.get(url + "todos",
+                             params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todo_dict if t.get("completed") is True]
+    completed = [t.get("title") for t in
+                 todo_dict if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user_dict.get("name"), len(completed), len(todo_dict)))
     [print("\t {}".format(c)) for c in completed]
